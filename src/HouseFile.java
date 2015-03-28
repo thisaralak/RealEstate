ca2d0b69bea1f8fc518086e2ced90a9e37f36127
Rasanjana <rnanayakkara1992@gmail.com>
2015-03-28 07:50:10 +0530

    Rasanjana-Changes to ListHouse

--- src/HouseFile.java
+++ src/HouseFile.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 
 public class HouseFile {
-
+	
+	ArrayList<ListHouse> listOfHouse = new ArrayList<>();
+	
+	
 }
--- src/ListHouse.java
+++ src/ListHouse.java
@@ -1,4 +1,66 @@
 
 public class ListHouse {
-
+	
+	//list ADT
+	
+	private int lotNumber;	
+	private String firstName;
+	private String lastName;
+	private double price;
+	private int squareFeet;
+	private int bedRooms;
+	
+	//constructor
+	
+	public ListHouse(int lotNumber, String firstName, String lastName,
+			double price, int squareFeet, int bedRooms) {
+		super();
+		this.lotNumber = lotNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.price = price;
+		this.squareFeet = squareFeet;
+		this.bedRooms = bedRooms;
+	}
+	
+	//getters and setters
+	
+	public int getLotNumber() {
+		return lotNumber;
+	}
+	public void setLotNumber(int lotNumber) {
+		this.lotNumber = lotNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getSquareFeet() {
+		return squareFeet;
+	}
+	public void setSquareFeet(int squareFeet) {
+		this.squareFeet = squareFeet;
+	}
+	public int getBedRooms() {
+		return bedRooms;
+	}
+	public void setBedRooms(int bedRooms) {
+		this.bedRooms = bedRooms;
+	}
+	
+	
 }
